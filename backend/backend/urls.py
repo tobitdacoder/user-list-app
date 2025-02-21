e0100576735cf734f91aa4d9b6388d7c53ae51c7
tobit bushenyula <115138001+tobitdacoder@users.noreply.github.com>
2025-02-21 14:26:52 +0000

    Testing API with Postman
    successfully runned the API test.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from users.views import UserListCreate
+from tasks.views import TaskListCreate 
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('users/',UserListCreate.as_view(),name='users-list'),
+    path('tasks/',TaskListCreate.as_view(),name='tasks-list'),
 ]
