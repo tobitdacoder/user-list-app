78e32b644a6244830b9e5c421eaca843529ae4b0
tobit bushenyula <115138001+tobitdacoder@users.noreply.github.com>
2025-02-21 14:00:09 +0000

    successfull migration
    here I have successfully completed the migration.

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+
+class Task(models.Model):
+    title=models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_to =models.ForeignKey(CustomUser, on_delete=models.CASCADE)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_set",  # Changing the related_name to prevent conflict
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions_set",  # Changing the related_name to prevent conflict
+        blank=True
+    )
